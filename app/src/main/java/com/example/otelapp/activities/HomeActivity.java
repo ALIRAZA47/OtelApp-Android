825702fdd0535bd913c817cbfcedab5a231b401f
Ali Raza Khan <raza2018@namal.edu.pk>
2021-08-10 09:26:53 +0500

    Added a home screen and Profile activity. Profiles can edited

--- app/src/main/java/com/example/otelapp/activities/HomeActivity.java
+++ app/src/main/java/com/example/otelapp/activities/HomeActivity.java
@@ -1,30 +1,46 @@
 package com.example.otelapp.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.otelapp.R;
 import com.example.otelapp.activities.signin_signup.LoginActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
 public class HomeActivity extends AppCompatActivity {
 
     //UI variables
+    AdView bannerAdView,bannerAdView2,bannerAdView3,bannerAdView4,bannerAdView5,bannerAdView6,bannerAdView7,bannerAdView8;
     NavigationView navigationView;
     MaterialToolbar appBar;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
     Button btnBookNow;
+    TextView cWeatherMain, cWeatherTemp, cWeatherTempMin, cWeatherTempMax;
 
     //Firebase variables
     FirebaseAuth mAuth;
@@ -33,6 +49,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        MobileAds.initialize(this); //initialize ads
+
 
         //variables declarations here
         mAuth = FirebaseAuth.getInstance();
@@ -47,8 +65,67 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void setupViews() {
-
+        requestAndSetWeather();
         setupDrawerLayout();
+        setupAds();
+
+
+
+    }
+
+    private void setupAds() {
+        bannerAdView = findViewById(R.id.adView);
+        bannerAdView2 = findViewById(R.id.adView2);
+        bannerAdView3 = findViewById(R.id.adView3);
+        bannerAdView4 = findViewById(R.id.adView4);
+        bannerAdView5 = findViewById(R.id.adView5);
+        bannerAdView6 = findViewById(R.id.adView6);
+        bannerAdView7 = findViewById(R.id.adView7);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        bannerAdView.loadAd(adRequest);
+        bannerAdView2.loadAd(adRequest);
+        bannerAdView3.loadAd(adRequest);
+        bannerAdView4.loadAd(adRequest);
+        bannerAdView5.loadAd(adRequest);
+        bannerAdView6.loadAd(adRequest);
+        bannerAdView7.loadAd(adRequest);
+    }
+
+    private void requestAndSetWeather() {
+        String reqURL = "https://api.openweathermap.org/data/2.5/weather?q=mianwali&appid=3beba2c5a71008c8501d6a9f70a0372b&units=metric";
+        cWeatherMain = findViewById(R.id.currentWeatherString);
+        cWeatherTemp = findViewById(R.id.currentWeatherTemperature);
+        cWeatherTempMin = findViewById(R.id.currentWeatherTempMin);
+        cWeatherTempMax = findViewById(R.id.currentWeatherTempMax);
+        DecimalFormat df = new DecimalFormat("##.#");
+
+        //request for weather
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, reqURL, null,
+                response -> {
+                    Log.e("Weather-", "requestAndSetWeather: -- > response received");
+                    // on success of request
+                    try {
+                        JSONObject jsonObject_main = response.getJSONObject("main");
+                        JSONArray weather_array = response.getJSONArray("weather");
+                        JSONObject jsonObject_weather = weather_array.getJSONObject(0);
+                        cWeatherMain.setText("" + jsonObject_weather.get("description"));
+                        cWeatherTemp.setText(df.format(Double.parseDouble(String.valueOf(jsonObject_main.get("temp")))) + getString(R.string.degree));
+                        cWeatherTempMin.setText("Min: " + jsonObject_main.get("temp_min"));
+                        cWeatherTempMax.setText("Max: " + jsonObject_main.get("temp_max"));
+
+
+                    } catch (JSONException e) {
+                        Log.e("Weather-Error", "requestAndSetWeather: --> " + e.getMessage());
+
+                    }
+
+                }, error -> {
+            //error related to JSON request
+            Log.e("Weather-Error1", "requestAndSetWeather: --> ", error);
+
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 
     //setup drawer navigation
@@ -70,6 +147,11 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 drawerLayout.closeDrawers();
+            }
+            if(item.getItemId() == R.id.navProfile){
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                //drawerLayout.closeDrawers();
             }
             return true;
         });
---
+++ app/src/main/java/com/example/otelapp/activities/ProfileActivity.java
@@ -0,0 +1,114 @@
+package com.example.otelapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.otelapp.R;
+import com.example.otelapp.models.User;
+import com.example.otelapp.utils.SharedPrefs;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private DatabaseReference dbRef;
+    EditText userName, userEmail, phone, cnic, address, gender;
+    Button btnEdit, btnSave;
+    User user = new User();
+    private SharedPrefs prefs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //initialize variables
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        prefs = new SharedPrefs(this);
+        String temp = prefs.getString("currentUser");
+        user = new Gson().fromJson(temp, User.class);
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        userName = findViewById(R.id.profileName);
+        userEmail = findViewById(R.id.profileEmail);
+        cnic = findViewById(R.id.profileCNIC);
+        address = findViewById(R.id.profileAddress);
+        gender = findViewById(R.id.profileGender);
+        phone = findViewById(R.id.profilePhone);
+        showUserInfo();
+
+        //edit profile
+        btnEdit = findViewById(R.id.btnEditProfile);
+        btnSave = findViewById(R.id.btnSaveProfile);
+        btnSave.setVisibility(View.GONE);
+
+        btnEdit.setOnClickListener(v -> {
+            //making them editable
+            userName.setEnabled(true);
+            userEmail.setEnabled(false);
+            phone.setEnabled(false);
+            address.setEnabled(true);
+            cnic.setEnabled(true);
+            gender.setEnabled(false);
+
+            btnEdit.setVisibility(View.GONE);
+            btnSave.setVisibility(View.VISIBLE);
+
+            btnSave.setOnClickListener(v1 -> {
+                //save user
+                //get updated fields
+                String newName = String.valueOf(userName.getText());
+                String newAddr = String.valueOf(address.getText());
+                String newCnic = String.valueOf(cnic.getText());
+                user.name = newName;
+                user.address = newAddr;
+                user.cnic = newCnic;
+
+                //.. update shared preferences' user
+                prefs.removeKeyPair("currentUser");
+                prefs.setString("currentUser", new Gson().toJson(user));
+
+                //change data on firebase
+                dbRef.child("main").child("users").child(user.phone).setValue(user);
+
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+
+
+            });
+
+        });
+
+
+    }
+
+    //shows user info of current user
+    private void showUserInfo() {
+
+
+        //making them non editable
+        userName.setEnabled(false);
+        userEmail.setEnabled(false);
+        phone.setEnabled(false);
+        address.setEnabled(false);
+        cnic.setEnabled(false);
+        gender.setEnabled(false);
+
+        //show details of user
+        userName.setText(String.valueOf(user.name));
+        userEmail.setText(String.valueOf(user.email));
+        address.setText(String.valueOf(user.address));
+        phone.setText(String.valueOf(user.phone));
+        gender.setText(String.valueOf(user.gender));
+        cnic.setText(String.valueOf(user.cnic));
+    }
+}
